09321357a48f09bc55345ef2115126ad1159a6a5
Max Rodriguez <max@vulkray.com>
2023-04-25 00:46:52 -0700

    src: Added temp main.cxx file to test emscripten compiler

---
+++ src/main.cxx
@@ -0,0 +1,18 @@
+/*
+ * astron.libwasm
+ * Copyright (c) 2023, Max Rodriguez. All rights reserved.
+ *
+ * All use of this software is subject to the terms of the revised BSD
+ * license. You should have received a copy of this license along
+ * with this source code in a file named "COPYING".
+ *
+ * @file main.cxx
+ * @author Max Rodriguez
+ * @date 2023-04-25
+ */
+
+#include <iostream>
+
+int main(unsigned int argc, char* argv[]) {
+    return 0;
+}
