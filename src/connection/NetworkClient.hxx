206c548f1dd4122f07c97319a962f38d5ab5d1ac
Max Rodriguez <max@vulkray.com>
2023-04-28 22:45:56 -0700

    src: Added Astron datagram / network client classes

---
+++ src/connection/NetworkClient.hxx
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2014, Astron Contributors. All rights reserved.
+ * Copyright (c) 2023, Max Rodriguez. All rights reserved.
+ *
+ * All use of this software is subject to the terms of the revised BSD
+ * license. You should have received a copy of this license along
+ * with this source code in a file named "COPYING".
+ */
+
+#ifndef ASTRON_LIBWASM_NETWORKCLIENT_HXX
+#define ASTRON_LIBWASM_NETWORKCLIENT_HXX
+
+#include <boost/asio.hpp>
+#include "../datagram/Datagram.hxx"
+
+class NetworkClient {
+protected:
+    NetworkClient();
+    NetworkClient(boost::asio::ip::tcp::socket *socket);
+    virtual ~NetworkClient();
+    void set_socket(boost::asio::ip::tcp::socket *socket);
+
+    virtual void network_datagram(Datagram &dg) = 0;
+    virtual void network_disconnect() = 0;
+    void network_send(Datagram &dg);
+    void do_disconnect();
+    bool is_connected();
+
+    boost::asio::ip::tcp::socket *m_socket;
+
+private:
+    void start_receive();
+    void handle_size(const boost::system::error_code &ec, size_t bytes_transferred);
+    void handle_data(const boost::system::error_code &ec, size_t bytes_transferred);
+
+    uint8_t m_size_buf[2];
+    uint8_t* m_data_buf;
+    dgsize_t m_data_size;
+    bool m_is_data;
+};
+
+#endif //ASTRON_LIBWASM_NETWORKCLIENT_HXX
---
+++ src/types.hxx
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2014, Astron Contributors. All rights reserved.
+ * Copyright (c) 2023, Max Rodriguez. All rights reserved.
+ *
+ * All use of this software is subject to the terms of the revised BSD
+ * license. You should have received a copy of this license along
+ * with this source code in a file named "COPYING".
+ */
+
+#ifndef ASTRON_LIBWASM_TYPES_HXX
+#define ASTRON_LIBWASM_TYPES_HXX
+
+#include <cstddef>
+#include <cstdint>
+
+/* Type definitions */
+#ifdef ASTRON_128BIT_CHANNELS
+#include "util/uint128.h"
+typedef uint128_t channel_t;
+typedef uint64_t doid_t;
+typedef uint64_t zone_t;
+#else
+typedef uint64_t channel_t;
+typedef uint32_t doid_t;
+typedef uint32_t zone_t;
+#endif
+
+/* Type limits */
+const channel_t CHANNEL_MAX = (channel_t)(-1);
+const doid_t DOID_MAX = (doid_t)(-1);
+const zone_t ZONE_MAX = (zone_t)(-1);
+const size_t ZONE_BITS = sizeof(zone_t) * 8;
+
+const unsigned int CHANNEL_SIZE_BYTES = 8;
+const unsigned int DOID_SIZE_BYTES = 4;
+const unsigned int ZONE_SIZE_BITS  = 32;
+const unsigned int ZONE_SIZE_BYTES = (ZONE_SIZE_BITS/8);
+
+/* DoId constants */
+const doid_t INVALID_DO_ID = 0;
+
+/* Channel constants */
+const channel_t INVALID_CHANNEL = 0;
+const channel_t CONTROL_MESSAGE = 1;
+const channel_t BCHAN_CLIENTS = 10;
+const channel_t BCHAN_STATESERVERS = 12;
+const channel_t BCHAN_DBSERVERS = 13;
+const channel_t PARENT_PREFIX = (channel_t(1) << ZONE_BITS);
+const channel_t DATABASE_PREFIX = (channel_t(2) << ZONE_BITS);
+
+/* Channel building methods */
+inline channel_t location_as_channel(doid_t parent, zone_t zone) {
+    return (channel_t(parent) << ZONE_BITS) | channel_t(zone);
+}
+inline channel_t parent_to_children(doid_t parent) {
+    return PARENT_PREFIX | channel_t(parent);
+}
+inline channel_t database_to_object(doid_t object) {
+    return DATABASE_PREFIX | channel_t(object);
+}
+
+#endif //ASTRON_LIBWASM_TYPES_HXX
