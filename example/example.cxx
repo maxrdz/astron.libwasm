17bafee4ecc37619635ff398bfc0186f4e80e93d
Max Rodriguez <max@vulkray.com>
2023-05-24 16:02:19 -0700

    cmake: If USE_PANDA, pass -sALLOW_MEMORY_GROWTH to ld

--- example/example.cxx
+++ example/example.cxx
@@ -22,7 +22,6 @@
 extern "C" void init_libOpenALAudio();
 extern "C" void init_libpnmimagetypes();
 extern "C" void init_libwebgldisplay();
-extern "C" void task_manager_poll();
 #endif
 
 #include "../src/client/ClientConnection.hxx"
@@ -39,20 +38,21 @@ void MyReactor::run(std::string astron_addr) {
 
 int main(int argc, char* argv[]) {
 #ifdef HAVE_PANDA
+    // Initialize Panda libraries
+    init_libOpenALAudio();
+    init_libpnmimagetypes();
+    init_libwebgldisplay();
     // Open a new window framework
     PandaFramework framework;
     framework.open_framework(argc, argv);
-    framework.set_window_title("My Panda3D Window");
     WindowFramework *window = framework.open_window();
+    // set clear buffer color
+    window->get_graphics_window()->set_clear_color(LVecBase4(0.5f, 0.5f, 0.5f, 1.0f)); // r g b a
 #endif
     MyReactor reactor;
     reactor.run("127.0.0.1"); // MD on loop back interface
 
 #ifdef HAVE_PANDA
-    init_libOpenALAudio();
-    init_libpnmimagetypes();
-    init_libwebgldisplay();
-    emscripten_set_main_loop(&task_manager_poll, 0, 0);
     framework.main_loop();
     framework.close_framework();
     return 0;
